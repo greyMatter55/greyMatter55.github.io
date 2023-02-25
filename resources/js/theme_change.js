7fb4dc6703b8bf3816aeca4699fadadac3743364
Jezza <jezza.millin@gmail.com>
2023-02-24 22:08:23 -0500

    Pages added

--- resources/js/theme_change.js
+++ resources/js/theme_change.js
@@ -3,15 +3,15 @@
 //Set theme on reload
 window.onload = set_theme();
 
-function set_theme(){
+function set_theme() {
   const cur_theme = window.localStorage.getItem("theme");
 
-  if(cur_theme !== null && cur_theme === "dark_theme"){
+  if (cur_theme !== null && cur_theme === "dark_theme") {
     document.body.className = "dark_theme";
-    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_dark.png'>"
-  } else if(cur_theme !== null && cur_theme === "light_theme"){
+    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_dark.png'>";
+  } else if (cur_theme !== null && cur_theme === "light_theme") {
     document.body.className = "light_theme";
-    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_light.png'>"
+    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_light.png'>";
   }
 }
 
@@ -22,16 +22,16 @@ const theme_mode = "";
 
 // Theme button Event Handlers
 themeButton.onclick = () => {
-    var theme_mode = document.body.className;
+  var theme_mode = document.body.className;
 
-    if (theme_mode === "light_theme") {
-        document.body.className = "dark_theme";
-        document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_dark.png'>"
-        window.localStorage.setItem('theme', 'dark_theme');
+  if (theme_mode === "light_theme") {
+    document.body.className = "dark_theme";
+    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_dark.png'>";
+    window.localStorage.setItem('theme', 'dark_theme');
 
-    } else if (theme_mode === "dark_theme") {
-        document.body.className = "light_theme";
-        document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_light.png'>"
-        window.localStorage.setItem('theme', 'light_theme');
-    }
+  } else if (theme_mode === "dark_theme") {
+    document.body.className = "light_theme";
+    document.getElementById("theme_btn").innerHTML = "<img src='./resources/img/theme_icon_light.png'>";
+    window.localStorage.setItem('theme', 'light_theme');
+  }
 };
