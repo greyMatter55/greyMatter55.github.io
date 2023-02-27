97965c02ac4581ff113bdd919ae5063793258ad9
Jezza <jezza.millin@gmail.com>
2023-02-26 20:47:36 -0500

    Script added for pages

--- resources/js/index_script.js
+++ resources/js/index_script.js
@@ -1,4 +1,13 @@
+document.getElementById("pg1").addEventListener("click", page_link("pg1"));
+document.getElementById("pg2").addEventListener("click", page_link("pg2"));
+document.getElementById("pg3").addEventListener("click", page_link("pg3"));
 
-    document.getElementById("myButton").onclick = function () {
-        location.href = "www.yoursite.com";
-    };
+function page_link(page_num){
+    if(page_num == "pg1"){
+        window.open("../webpages/resume.html", "_self");
+    } else if(page_num == "pg2"){
+        window.open("../webpages/projects.html", "_self");
+    } else if(page_num == "pg3"){
+        window.open("../webpages/about.html", "_self");
+    }    
+}
---
+++ resources/js/projects_scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("proj_b7").addEventListener("click", p7_link);
+
+function p7_link(){
+    window.open("./Financial Planner/summary.html", "_self");
+}
