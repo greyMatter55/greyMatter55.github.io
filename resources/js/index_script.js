7c6006a4984140ebcf55480fdc2c1b9553ed3873
Jezza <jezza.millin@gmail.com>
2023-02-26 21:19:58 -0500

    FIxed page links

--- resources/js/index_script.js
+++ resources/js/index_script.js
@@ -1,20 +1,25 @@
-// document.body.getElementById("pg1").addEventListener("click", page_link("pg1"));
-// document.body.getElementById("pg2").addEventListener("click", page_link("pg2"));
-document.getElementById("pg2").addEventListener("click", test);
-
-
-function test(){
-    window.open("./webpages/projects.html", "_self");
-}
 
 
 
-// function page_link(page_num){
-//     if(page_num === "pg1"){
-//         window.open("./webpages/resume.html", "_blank");
-//     } else if(page_num === "pg2"){
-//         window.open("./webpages/projects.html", "_self");
-//     } else if(page_num === "pg3"){
-//         window.open("./webpages/about.html", "_self");
-//     }    
+// document.getElementById("pg2").addEventListener("click", test);
+
+
+// function test(){
+//     window.open("./webpages/projects.html", "_self");
 // }
+
+
+
+document.getElementById("pg1").addEventListener("click", () => { page_link("pg1") });
+document.getElementById("pg2").addEventListener("click", () => { page_link("pg2") });
+document.getElementById("pg3").addEventListener("click", () => { page_link("pg3") });
+
+function page_link(page_num) {
+    if (page_num === "pg1") {
+        window.open("./webpages/resume.html", "_blank");
+    } else if (page_num === "pg2") {
+        window.open("./webpages/projects.html", "_self");
+    } else if (page_num === "pg3") {
+        window.open("./webpages/about.html", "_self");
+    }
+}
